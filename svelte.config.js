b5833c4135dff6e4dde3cbc6069d943169fddba0
Markus Hilbert <markus.hilbert@iham.at>
2022-02-01 20:15:48 +0100

    commit

---
+++ svelte.config.js
@@ -0,0 +1,25 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: null
+		}),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		},
+		prerender: {
+			createIndexFiles: false,
+			crawl: true,
+			enabled: true,
+		}
+	}
+};
+
+export default config;
