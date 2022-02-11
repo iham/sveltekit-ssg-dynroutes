945e17056ef0f552fe49c86cf50a75066f17c4ab
Markus Hilbert <markus.hilbert@iham.at>
2022-02-11 11:28:08 +0100

    updated

---
+++ docs/_app/pages/about.svelte-8921358e.js
@@ -0,0 +1,3 @@
+import{S as r,i,s as o,e as l,a as c,c as f,F as t,f as u}from"../chunks/vendor-4af5f350.js";function d(a){let e;return{c(){e=l("main"),e.innerHTML=`<h1>ABOUT</h1> 
+  <hr/> 
+  <div>A website to find user profiles</div>`,c(e,"class","svelte-kss3l6")},m(s,n){f(s,e,n)},p:t,i:t,o:t,d(s){s&&u(e)}}}class p extends r{constructor(e){super();i(this,e,null,d,o,{})}}export{p as default};
